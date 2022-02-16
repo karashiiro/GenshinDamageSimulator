438f822d820c780daf96fb83c5c994a0630be8e0
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-02-15 22:20:04 -0800

    Track event results and fix StepBack

--- GenshinDamageSimulator/Simulator.fs
+++ GenshinDamageSimulator/Simulator.fs
@@ -8,6 +8,7 @@ exception NoHistoryException of string
 type SimulationState =
     { Combatants: Map<uint32, (BattleNpc * BattleNpcState)>
       Party: Party
+      LastEventResult: GameEventResult option
       History: SimulationState list }
 
 module Simulator =
@@ -28,16 +29,17 @@ module Simulator =
             | (Some (defender, defenderState), Some (GameEventResult.DamageResult r))
                 -> Some(defender, { defenderState with Hp = defenderState.Hp - r.DamageAmount })
             | _ -> None
-        { state with Combatants = state.Combatants.Change (defenderId, updateFn); History = state :: state.History }
+        { state with Combatants = state.Combatants.Change (defenderId, updateFn); LastEventResult = eventResult; History = state :: state.History }
 
     let StepBack state =
         match state.History with
-        | head :: tail -> { Combatants = head.Combatants; Party = head.Party; History = tail }
+        | _ :: nextHead :: _ -> nextHead
         | _ -> state
 
     let Create =
         { Combatants = Map.empty
           Party = []
+          LastEventResult = None
           History = [] }
 
 // This is the C# interface for the simulator.
