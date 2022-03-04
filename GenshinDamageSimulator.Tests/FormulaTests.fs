00404a7e573bb161460daa6b06f089a912f1c581
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-03-04 10:02:03 -0800

    Fix test

--- GenshinDamageSimulator.Tests/FormulaTests.fs
+++ GenshinDamageSimulator.Tests/FormulaTests.fs
@@ -8,8 +8,8 @@ module FormulaTests =
     [<Fact>]
     let ``Test that enemy defense multipliers are calculated correctly``() =
         let levelChar = 90u
-        let levelEnemy = 81u
+        let defEnemy = 500f + 5f * 81f
         let defReduction = 0f
         let defIgnore = 0f
-        let actual = Formulas.calcDefenseMultiplier levelChar levelEnemy defReduction defIgnore
+        let actual = Formulas.calcDefenseMultiplier levelChar defEnemy defReduction defIgnore
         abs (actual - 0.51212938f) |> should be (lessThan 0.01f)
