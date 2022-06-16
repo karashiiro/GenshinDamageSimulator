1e997c6950025aa2b93b34bb7bf5c9dd612f0260
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-06-16 12:53:02 -0700

    Split Elements module into Auras and Reactions

--- Scryglass/auras.fs
+++ Scryglass/auras.fs
@@ -1,9 +1,9 @@
 ﻿namespace Scryglass
 
-open Stats
-open Time
+module Auras =
+    open Stats
+    open Time
 
-module Elements =
     type GaugeUnits = float
     type InternalCooldownGroupId = uint
     type InternalCooldown = Seconds * InternalCooldownGroupId
@@ -44,34 +44,3 @@ module Elements =
           Anemo: AnemoAuraData option
           Geo: GeoAuraData option
           Dendro: DendroAuraData option }
-
-    type StrongVaporize = PyroAuraData * HydroAuraData
-    type WeakVaporize = HydroAuraData * PyroAuraData
-    type StrongMelt = CryoAuraData * PyroAuraData
-    type WeakMelt = PyroAuraData * CryoAuraData
-
-    type Overload = PyroAuraData * ElectroAuraData
-    type Superconduct = ElectroAuraData * CryoAuraData
-    type ElectroCharged = ElectroAuraData * HydroAuraData
-    type Shatter = CryoAuraData  // Frozen <- Heavy
-    type Frozen = HydroAuraData * CryoAuraData
-    type Swirl = VolatileAura * AnemoAuraData
-    type Crystallize = VolatileAura * GeoAuraData
-    type Burning = PyroAuraData * DendroAuraData
-
-    type Reaction =
-        | StrongVaporize of StrongVaporize
-        | Overload of Overload
-        | WeakMelt of WeakMelt
-        | WeakVaporize of WeakVaporize
-        | ElectroCharged of ElectroCharged
-        | Frozen of Frozen
-        | Shatter of Shatter
-        | Superconduct of Superconduct
-        | StrongMelt of StrongMelt
-        | Swirl of Swirl
-        | Crystallize of Crystallize
-        | Burning of Burning
-
-    type ApplyAura = ElementAuraState -> Aura -> ElementAuraState * Reaction option
-    type Elapse = ElementAuraState -> Seconds -> ElementAuraState
---
+++ Scryglass/reactions.fs
@@ -0,0 +1,36 @@
+﻿namespace Scryglass
+
+module Reactions =
+    open Auras
+    open Time
+
+    type StrongVaporize = PyroAuraData * HydroAuraData
+    type WeakVaporize = HydroAuraData * PyroAuraData
+    type StrongMelt = CryoAuraData * PyroAuraData
+    type WeakMelt = PyroAuraData * CryoAuraData
+
+    type Overload = PyroAuraData * ElectroAuraData
+    type Superconduct = ElectroAuraData * CryoAuraData
+    type ElectroCharged = ElectroAuraData * HydroAuraData
+    type Shatter = CryoAuraData  // Frozen <- Heavy
+    type Frozen = HydroAuraData * CryoAuraData
+    type Swirl = VolatileAura * AnemoAuraData
+    type Crystallize = VolatileAura * GeoAuraData
+    type Burning = PyroAuraData * DendroAuraData
+
+    type Reaction =
+        | StrongVaporize of StrongVaporize
+        | Overload of Overload
+        | WeakMelt of WeakMelt
+        | WeakVaporize of WeakVaporize
+        | ElectroCharged of ElectroCharged
+        | Frozen of Frozen
+        | Shatter of Shatter
+        | Superconduct of Superconduct
+        | StrongMelt of StrongMelt
+        | Swirl of Swirl
+        | Crystallize of Crystallize
+        | Burning of Burning
+
+    type ApplyAura = ElementAuraState -> Aura -> ElementAuraState * Reaction option
+    type Elapse = ElementAuraState -> Seconds -> ElementAuraState
